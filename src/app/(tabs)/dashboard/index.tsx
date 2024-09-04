4391b903db02097c054507a03a0eff15d7025e90
Jorge Sousa <jorgeferartsousa@gmail.com>
2024-09-03 22:24:44 -0300

    Feature: create chart in dashboard

--- src/app/(tabs)/dashboard/index.tsx
+++ src/app/(tabs)/dashboard/index.tsx
@@ -1,20 +1,32 @@
 import React, { Component, useEffect, useState } from 'react'
-import { SafeAreaView, Text, TouchableOpacity, View } from 'react-native'
+import { SafeAreaView, Text, TouchableOpacity, useColorScheme, View } from 'react-native'
 import { supabase } from '../../../database/supabase'
 import { Session } from '@supabase/supabase-js'
-import { CartesianChart, CartesianChartRenderArg } from "victory-native";
+import { CartesianChart, Bar, Line, Area } from "victory-native";
 import Login from '../../login'
-import Container from '../../../components/container'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import { router } from 'expo-router';
+import { LinearGradient, useFont, vec } from '@shopify/react-native-skia';
+
+const mono = require('../../../../assets/fonts/SpaceMono-Regular.ttf');
 
 const Dashboard = () => {
     const [session, setSession] = useState<Session | null>(null)
+    
+    const font = useFont(mono, 12);
+    const colorMode = useColorScheme();
+ 
+    const labelColor = colorMode === "dark" ? "#fff" : "#000";   
+    const lineColor = colorMode === "dark" ? "lightgrey" : "#000";
 
-    const DATA = Array.from({ length: 31 }, (_, i) => ({
-        day: i,
-        highTmp: 40 + 30 * Math.random(),
-      }));
+    const DATA = [
+        {day: "08-01", highTmp: 100},
+        {day: "08-02", highTmp: 300},
+        {day: "08-03", highTmp: 400},
+        {day: "08-04", highTmp: 600},  
+        {day: "08-05", highTmp: 1000},
+        {day: "08-06", highTmp: 700},
+    ];
 
     const signOut = () => {
         supabase.auth.signOut();
@@ -40,8 +52,47 @@ const Dashboard = () => {
                             <Ionicons name="exit" size={24} color="#72cc" />
                         </TouchableOpacity>
                     </View>
-                    <View className='mt-80 justify-center items-center'>
-                        <Text className='text-purple-700 text-center font-bold text-3xl'>Dashboard</Text>
+                    <Text className='text-purple-700 text-center font-bold text-md mb-4'>Ultimas Compras</Text>
+                    <View className='tems-center justify-center px-8 mt-2 w-full'>
+                        <View style={{ height: 300, width: "auto" }}>
+                            <CartesianChart
+                                data={DATA}                
+                                xKey={"day"}               
+                                yKeys={["highTmp"]}
+                                domainPadding={{ top: 30 }}
+                                axisOptions={{
+                                    font,
+                                    labelColor,
+                                    lineColor
+                                }}    
+                            >
+                                {({ points, chartBounds }) => {
+                                    return (
+                                        <>
+                                            <Line
+                                                points={points.highTmp}
+                                                color={"#ac24db"}
+                                                strokeWidth={3}
+                                                animate={{ type: "timing", duration: 500 }}
+                                            />
+                                            <Area
+                                                points={points.highTmp}
+                                                y0={chartBounds.bottom}
+                                                animate={{ type: "timing", duration: 500 }}
+                                                color={"#ac24db"}
+                                                opacity={0.2}
+                                            >
+                                                <LinearGradient
+                                                    start={vec(chartBounds.bottom, 200)}
+                                                    end={vec(chartBounds.bottom, chartBounds.bottom)}
+                                                    colors={["#ac24db", "#ac24db00"]}
+                                                />
+                                            </Area>
+                                      </>
+                                    );
+                                }}
+                            </CartesianChart>
+                        </View>
                     </View>
                 </SafeAreaView>
             ): (
